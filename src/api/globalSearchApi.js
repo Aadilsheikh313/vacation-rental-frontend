1b46c9fc61c1b34c5f35872cae948b1a3b96365a
nebula-aadil <aadil.shaikh@nebula-technology.com>
2025-08-16 12:51:21 +0530

    update code

--- src/api/globalSearchApi.js
+++ src/api/globalSearchApi.js
@@ -1,9 +1,11 @@
+// globalSearchApi.js
 import { clientServer } from "../config/axios";
 
+// 🔍 Global Search API Call
 export const globalSearchApi = async (filters = {}) => {
   try {
-    const response = await clientServer.get("/api/search", {
-      params: filters,   // yahan pe query params jayenge
+    const response = await clientServer.get("/api/search/global", {
+      params: filters,   // yahan query params jayenge
     });
     return response.data;
   } catch (error) {
